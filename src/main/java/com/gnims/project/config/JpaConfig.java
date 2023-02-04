fc89f0c3fefeecc03d47b6c7688739e7f9a60247
JxxHxxx <leesin5498@naver.com>
2023-02-04 20:31:57 +0900

    Feat #15 Auditor 구현체 및 Jpa 설정 변경

---
+++ src/main/java/com/gnims/project/config/JpaConfig.java
@@ -0,0 +1,17 @@
+package com.gnims.project.config;
+
+import com.gnims.project.util.Auditor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<Long> auditorProvider() {
+        return new Auditor();
+    }
+}
