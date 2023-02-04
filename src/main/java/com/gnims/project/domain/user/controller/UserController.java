f3a2ab6932c622806dc141b28cf0e1e20c29a2f2
yuminwoo <ymw1023@naver.com>
2023-02-04 15:28:42 +0900

    Feat #3 로그인
    변수명 memberRepository를 uesrRepository로 변경

--- src/main/java/com/gnims/project/domain/user/controller/UserController.java
+++ src/main/java/com/gnims/project/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.gnims.project.domain.user.controller;
 
+import com.gnims.project.domain.user.dto.LoginRequestDto;
 import com.gnims.project.domain.user.dto.MessageResponseDto;
 import com.gnims.project.domain.user.dto.SignupRequestDto;
 import com.gnims.project.domain.user.service.UserService;
@@ -7,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +20,11 @@ public class UserController {
     public MessageResponseDto signup(@RequestBody SignupRequestDto request) {
 
         return userService.signup(request);
+    }
+
+    @PostMapping("/auth/login")
+    public MessageResponseDto login(@RequestBody LoginRequestDto request, HttpServletResponse response) {
+
+        return userService.login(request, response);
     }
 }
--- src/main/java/com/gnims/project/domain/user/service/UserService.java
+++ src/main/java/com/gnims/project/domain/user/service/UserService.java
@@ -1,33 +1,55 @@
 package com.gnims.project.domain.user.service;
 
+import com.gnims.project.domain.user.dto.LoginRequestDto;
 import com.gnims.project.domain.user.dto.MessageResponseDto;
 import com.gnims.project.domain.user.dto.SignupRequestDto;
 import com.gnims.project.domain.user.entity.User;
 import com.gnims.project.domain.user.repository.UserRepository;
+import com.gnims.project.security.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository memberRepository;
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
 
     @Transactional
     public MessageResponseDto signup(SignupRequestDto request) {
 
         //이메일 중복 체크
-        if(memberRepository.findByEmail(request.getEmail()).isPresent()) {
+        if(userRepository.findByEmail(request.getEmail()).isPresent()) {
             throw new IllegalArgumentException("이미 등록된 이메일 입니다");
         }
 
         //닉네임 중복 체크
-        if(memberRepository.findByUsername(request.getNickname()).isPresent()) {
+        if(userRepository.findByUsername(request.getNickname()).isPresent()) {
             throw new IllegalArgumentException("중복된 닉네임 입니다");
         }
 
-        memberRepository.save(new User(request));
+        userRepository.save(new User(request));
 
         return new MessageResponseDto("회원가입 성공!");
+    }
+
+    @Transactional
+    public MessageResponseDto login(LoginRequestDto request, HttpServletResponse response) {
+        String email = request.getEmail();
+        String password = request.getPassword();
+
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+
+        if(!user.getPassword().equals(password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername()));
+        return new MessageResponseDto(user.getUsername() + "님 로그인 완료");
     }
 }
