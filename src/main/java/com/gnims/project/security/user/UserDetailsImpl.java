0bc5b96bd694bba1039605c4ea7738f94d4e8f7e
yuminwoo <ymw1023@naver.com>
2023-02-04 14:35:12 +0900

    Feat #10 UserDetails, UserDetailsService 구현

---
+++ src/main/java/com/gnims/project/security/user/UserDetailsImpl.java
@@ -0,0 +1,57 @@
+package com.gnims.project.security.user;
+
+import com.gnims.project.domain.user.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+    private final String username;
+
+    public UserDetailsImpl(User user, String username) {
+        this.user = user;
+        this.username = username;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
