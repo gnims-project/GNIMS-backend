2850cdd2273bcbc34b33c6b51d13725f1923793f
yuminwoo <ymw1023@naver.com>
2023-02-04 19:15:24 +0900

    Feat #17 User 기능에 Kakao 회원 추가

--- src/main/java/com/gnims/project/domain/user/controller/UserController.java
+++ src/main/java/com/gnims/project/domain/user/controller/UserController.java
@@ -1,21 +1,47 @@
 package com.gnims.project.domain.user.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.gnims.project.domain.user.dto.LoginRequestDto;
 import com.gnims.project.domain.user.dto.MessageResponseDto;
 import com.gnims.project.domain.user.dto.SignupRequestDto;
+import com.gnims.project.domain.user.kakao.KakaoService;
 import com.gnims.project.domain.user.service.UserService;
+import com.gnims.project.security.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final KakaoService kakaoService;
 
     @PostMapping("/auth/signup")
     public MessageResponseDto signup(@RequestBody SignupRequestDto request) {
 
         return userService.signup(request);
+    }
+
+    @PostMapping("/auth/login")
+    public MessageResponseDto login(@RequestBody LoginRequestDto request, HttpServletResponse response) {
+
+        return userService.login(request, response);
+    }
+
+    @GetMapping("/api/user/kakao/callback")
+    public MessageResponseDto kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+
+        // code: 카카오 서버로부터 받은 인가 코드
+        String createToken = kakaoService.kakaoLogin(code);
+
+        // Cookie 생성 및 직접 브라우저에 Set
+        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, createToken.substring(7));
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return new MessageResponseDto("성공했나요? 쿠키를 보세용 -> Authorization");
     }
 }
--- src/main/java/com/gnims/project/domain/user/entity/User.java
+++ src/main/java/com/gnims/project/domain/user/entity/User.java
@@ -24,9 +24,24 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Column
+    private Long kakaoId;
+
     public User(SignupRequestDto request) {
         this.username = request.getNickname();
         this.email = request.getEmail();
         this.password = request.getPassword();
+    }
+
+    public User(String username, Long kakaoId, String email, String password) {
+        this.username = username;
+        this.kakaoId = kakaoId;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User kakaoIdUpdate(Long kakaoId) {
+        this.kakaoId = kakaoId;
+        return this;
     }
 }
