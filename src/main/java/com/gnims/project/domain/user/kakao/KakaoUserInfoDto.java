a16e61e5f46906e60db9f2787ae85e44ddcfaeab
yuminwoo <ymw1023@naver.com>
2023-02-04 19:14:38 +0900

    Feat #17 KakaoService, Dto 추가

---
+++ src/main/java/com/gnims/project/domain/user/kakao/KakaoUserInfoDto.java
@@ -0,0 +1,18 @@
+package com.gnims.project.domain.user.kakao;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class KakaoUserInfoDto {
+    private Long id;
+    private String nickname;
+    private String email;
+
+    public KakaoUserInfoDto(Long id, String nickname, String email) {
+        this.id = id;
+        this.nickname = nickname;
+        this.email = email;
+    }
+}
