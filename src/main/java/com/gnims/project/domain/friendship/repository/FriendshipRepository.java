266b54548e8375e5ad83685997e501ebe3557af3
JxxHxxx <leesin5498@naver.com>
2023-02-06 14:17:02 +0900

    Refactor #22 팔로잉 하기/취소 API
    FollowStatus 클래스 명 변경에 따른 변화

--- src/main/java/com/gnims/project/domain/friendship/repository/FriendshipRepository.java
+++ src/main/java/com/gnims/project/domain/friendship/repository/FriendshipRepository.java
@@ -1,7 +1,7 @@
 package com.gnims.project.domain.friendship.repository;
 
 import com.gnims.project.domain.friendship.entity.Friendship;
-import com.gnims.project.domain.friendship.entity.FriendshipStatus;
+import com.gnims.project.domain.friendship.entity.FollowStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,6 +9,6 @@ import java.util.Optional;
 
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
 
-    List<Friendship> findAllByCreateByAndStatusIs(Long id, FriendshipStatus status);
+    List<Friendship> findAllByCreateByAndStatusIs(Long id, FollowStatus status);
     Optional<Friendship> findByFollower_IdAndCreateBy(Long followerId, Long MyId);
 }
