24c4824a71a95f66bd3b99220e1dcaed05344b98
yuminwoo <ymw1023@naver.com>
2023-02-07 11:41:02 +0900

    Fix #3 로그인시 유저 정보를 리턴 하도록 변경

--- src/main/java/com/gnims/project/domain/user/controller/UserController.java
+++ src/main/java/com/gnims/project/domain/user/controller/UserController.java
@@ -2,16 +2,15 @@ package com.gnims.project.domain.user.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.gnims.project.domain.user.dto.LoginRequestDto;
+import com.gnims.project.domain.user.dto.LoginResponseDto;
 import com.gnims.project.domain.user.dto.MessageResponseDto;
 import com.gnims.project.domain.user.dto.SignupRequestDto;
 import com.gnims.project.domain.user.kakao.KakaoService;
 import com.gnims.project.domain.user.naver.NaverService;
 import com.gnims.project.domain.user.service.UserService;
-import com.gnims.project.security.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
@@ -34,30 +33,16 @@ public class UserController {
     }
 
     @GetMapping("/api/user/kakao/callback")
-    public MessageResponseDto kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
+    public LoginResponseDto kakaoLogin(@RequestParam String code, HttpServletResponse response) throws JsonProcessingException {
 
         // code: 카카오 서버로부터 받은 인가 코드
-        String createToken = kakaoService.kakaoLogin(code);
-
-        // Cookie 생성 및 직접 브라우저에 Set
-        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, createToken.substring(7));
-        cookie.setPath("/");
-        response.addCookie(cookie);
-
-        return new MessageResponseDto("카카오 성공했나요? 쿠키를 보세용 -> Authorization");
+        return kakaoService.kakaoLogin(code, response);
     }
 
     @GetMapping("/api/user/naver/callback")
-    public MessageResponseDto naverLogin(@RequestParam String code, @RequestParam String state, HttpServletResponse response) throws JsonProcessingException {
+    public LoginResponseDto naverLogin(@RequestParam String code, @RequestParam String state, HttpServletResponse response) throws JsonProcessingException {
 
-        // code: 카카오 서버로부터 받은 인가 코드
-        String createToken = naverService.naverLogin(code, state);
-
-        // Cookie 생성 및 직접 브라우저에 Set
-        Cookie cookie = new Cookie(JwtUtil.AUTHORIZATION_HEADER, createToken.substring(7));
-        cookie.setPath("/");
-        response.addCookie(cookie);
-
-        return new MessageResponseDto("네이버 성공했나요? 쿠키를 보세용 -> Authorization");
+        // code: 네이버 서버로부터 받은 인가 코드
+        return naverService.naverLogin(code, state, response);
     }
 }
