f4595699434037593c7bc18638e92fcf1a67d0bc
JxxHxxx <leesin5498@naver.com>
2023-02-07 14:11:45 +0900

    Feat #27 Friendship 엔티티 변경
    1. BaseEntity -> TimeStamped 상속
    2. User 엔티티 두개가 포함됨

--- src/main/java/com/gnims/project/domain/friendship/entity/Friendship.java
+++ src/main/java/com/gnims/project/domain/friendship/entity/Friendship.java
@@ -1,7 +1,7 @@
 package com.gnims.project.domain.friendship.entity;
 
 import com.gnims.project.domain.user.entity.User;
-import com.gnims.project.util.BaseEntity;
+import com.gnims.project.util.TimeStamped;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,26 +12,33 @@ import static com.gnims.project.domain.friendship.entity.FollowStatus.*;
 @Getter
 @Entity
 @NoArgsConstructor
-public class Friendship extends BaseEntity {
+public class Friendship extends TimeStamped {
 
     @Id @Column(name = "friendship_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //자기 자신
     @ManyToOne
-    @JoinColumn(name = "follower_id")
-    private User follower;
+    @JoinColumn(name = "myself_id")
+    private User myself;
+
+    //팔로우 등록할 친구
+    @ManyToOne
+    @JoinColumn(name = "following_id")
+    private User following;
 
     @Enumerated(value = EnumType.STRING)
     private FollowStatus status;
 
-    public Friendship(User follower) {
-        this.follower = follower;
+    public Friendship(User myself, User following) {
+        this.myself = myself;
+        this.following = following;
         this.status = INIT;
     }
 
     public String receiveUsername() {
-        return this.follower.getUsername();
+        return this.following.getUsername();
     }
 
     public boolean isActive() {
@@ -45,8 +52,9 @@ public class Friendship extends BaseEntity {
     public void changeStatus(FollowStatus status) {
         this.status = status;
     }
+
     public Long receiveFollowId() {
-        return this.follower.getId();
+        return this.following.getId();
     }
 
 }
