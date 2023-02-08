a9465a8502a24b4040346a7722abbb8220af9385
JxxHxxx <leesin5498@naver.com>
2023-02-09 02:33:49 +0900

    Feat #40 스케줄 조회 API

--- src/main/java/com/gnims/project/domain/event/repository/EventRepository.java
+++ src/main/java/com/gnims/project/domain/event/repository/EventRepository.java
@@ -3,5 +3,8 @@ package com.gnims.project.domain.event.repository;
 import com.gnims.project.domain.event.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EventRepository extends JpaRepository<Event, Long> {
+    Optional<Event> findBySubject(String subject);
 }
