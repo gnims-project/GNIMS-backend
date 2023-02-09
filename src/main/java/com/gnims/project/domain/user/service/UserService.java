0706ad7883b60ee9c7a565877536935c3bbcea32
yuminwoo <ymw1023@naver.com>
2023-02-09 15:04:04 +0900

    Feat #4 닉네임, 이메일 중복체크 추가

--- src/main/java/com/gnims/project/domain/user/service/UserService.java
+++ src/main/java/com/gnims/project/domain/user/service/UserService.java
@@ -1,9 +1,6 @@
 package com.gnims.project.domain.user.service;
 
-import com.gnims.project.domain.user.dto.LoginRequestDto;
-import com.gnims.project.domain.user.dto.LoginResponseDto;
-import com.gnims.project.domain.user.dto.MessageResponseDto;
-import com.gnims.project.domain.user.dto.SignupRequestDto;
+import com.gnims.project.domain.user.dto.*;
 import com.gnims.project.domain.user.entity.User;
 import com.gnims.project.domain.user.repository.UserRepository;
 import com.gnims.project.security.jwt.JwtUtil;
@@ -14,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -21,31 +20,65 @@ public class UserService {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
     private final PasswordEncoder passwordEncoder;
+    private String pt = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{8,20}$";
 
     @Transactional
     public MessageResponseDto signup(SignupRequestDto request) {
 
+        String email = "Gnims." + request.getSocialCode().getValue() + "." + request.getEmail();
+
         //이메일 중복 체크
-        if(userRepository.findByEmail(request.getEmail()).isPresent()) {
+        if(userRepository.findByEmail(email).isPresent()) {
             throw new IllegalArgumentException("이미 등록된 이메일 입니다");
         }
 
         //닉네임 중복 체크
-        if(userRepository.findByUsername(request.getNickname()).isPresent()) {
+        if(userRepository.findByNickname(request.getNickname()).isPresent()) {
             throw new IllegalArgumentException("중복된 닉네임 입니다");
         }
 
-        //비밀번호 암호화
-        String password = passwordEncoder.encode(request.getPassword());
+        String password = request.getPassword();
 
-        userRepository.save(new User(request.getNickname(), request.getEmail(), password));
+        //소셜 회원가입
+        if (request.getPassword() == null) {
+            password = UUID.randomUUID().toString();
+        }
+
+        //일반 회원가입
+        else if(!Pattern.matches(pt, password)) {
+            throw new IllegalArgumentException("영문자와 숫자가 포함된 8 ~ 20 자리의 비밀번호만 가능합니다.");
+        }
+
+        //비밀번호 암호화
+        password = passwordEncoder.encode(password);
+
+        userRepository.save(new User(request.getUsername(), request.getNickname(), email, password));
 
         return new MessageResponseDto("회원가입 성공!");
     }
 
+    public MessageResponseDto checkNickname(NicknameDto request) {
+
+        if (userRepository.findByNickname(request.getNickname()).isPresent()) {
+            return new MessageResponseDto("중복된 닉네임 입니다");
+        }
+        return new MessageResponseDto("사용 가능한 닉네임 입니다");
+    }
+
+    public MessageResponseDto checkEmail(EmailDto request) {
+
+        String email = "Gnims." + request.getSocialCode().getValue() + "." + request.getEmail();
+
+        if (userRepository.findByEmail(email).isPresent()) {
+            return new MessageResponseDto("이미 등록된 이메일 입니다");
+        }
+        return new MessageResponseDto("사용 가능한 이메일 입니다");
+    }
+
+    //code : Auth, Kakao, Naver
     public LoginResponseDto login(LoginRequestDto request, HttpServletResponse response) {
 
-        String email = request.getEmail();
+        String email = "Gnims." + request.getSocialCode().getValue() + "." + request.getEmail();
 
         User user = userRepository.findByEmail(email).orElseThrow(
                 () -> new BadCredentialsException("등록된 사용자가 없습니다.")
@@ -56,8 +89,8 @@ public class UserService {
             throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
         }
 
-        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername()));
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getNickname()));
 
-        return new LoginResponseDto(user.getUsername(), user.getEmail());
+        return new LoginResponseDto(user.getNickname(), request.getEmail(), request.getSocialCode());
     }
 }
