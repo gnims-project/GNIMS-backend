006b5744ba2128627464a488f533d895885e74f2
yuminwoo <ymw1023@naver.com>
2023-02-09 15:08:48 +0900

    Feat #4 User 회원가입, 로그인 시 socialCode를 받게 수정

--- src/main/java/com/gnims/project/domain/user/dto/LoginRequestDto.java
+++ src/main/java/com/gnims/project/domain/user/dto/LoginRequestDto.java
@@ -1,9 +1,11 @@
 package com.gnims.project.domain.user.dto;
 
+import com.gnims.project.domain.user.entity.SocialCode;
 import lombok.Getter;
 
 @Getter
 public class LoginRequestDto {
+    private SocialCode socialCode;
     private String email;
     private String password;
 }
