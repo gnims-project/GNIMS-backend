6b7ab3adb09dabc8f6926ddcb1f524681f4734d5
JxxHxxx <leesin5498@naver.com>
2023-02-11 13:42:58 +0900

    Fix #66 스케줄 등록 API 테스트 케이스 수정

---
+++ src/main/java/com/gnims/project/security/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package com.gnims.project.security.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gnims.project.exception.dto.ExceptionResponseMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static ExceptionResponseMessage exceptionMessage = new ExceptionResponseMessage(HttpStatus.UNAUTHORIZED.value(), "로그인이 필요한 서비스입니다.");
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        ServletOutputStream outputStream = response.getOutputStream();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(outputStream, exceptionMessage);
+        outputStream.flush();
+
+    }
+}
