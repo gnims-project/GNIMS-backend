d1d71a4fee1dcad63a0e48e12015d19d622b61cb
JxxHxxx <leesin5498@naver.com>
2023-02-17 16:23:30 +0900

    Test #99 #68 일정 수락 API 테스트 코드 작성

--- src/test/java/com/gnims/project/domain/schedule/service/ScheduleAcceptTest.java
+++ src/test/java/com/gnims/project/domain/schedule/service/ScheduleAcceptTest.java
@@ -1,8 +1,8 @@
 package com.gnims.project.domain.schedule.service;
 
+import com.gnims.project.domain.event.entity.Event;
 import com.gnims.project.domain.event.repository.EventRepository;
 import com.gnims.project.domain.schedule.entity.Schedule;
-import com.gnims.project.domain.schedule.entity.ScheduleStatus;
 import com.gnims.project.domain.schedule.repository.ScheduleRepository;
 import com.gnims.project.domain.user.repository.UserRepository;
 import org.assertj.core.api.Assertions;
@@ -19,13 +19,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import static com.gnims.project.domain.schedule.entity.ScheduleStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Transactional
 @AutoConfigureMockMvc
 @SpringBootTest
 public class ScheduleAcceptTest {
@@ -51,6 +54,143 @@ public class ScheduleAcceptTest {
 
     @BeforeEach
     void beforeEach() throws Exception {
+        makeUser();
+
+        MvcResult result = getLoginResult("{\"email\": \"ddalgi@gmail.com\", \"password\": \"123456aA9\"}");
+        hostToken = result.getResponse().getHeader("Authorization");
+
+        MvcResult result2 = getLoginResult("{\"email\": \"danguen@gmail.com\", \"password\": \"123456aA9\"}");
+        inviteeToken = result2.getResponse().getHeader("Authorization");
+
+        Long hostId = userRepository.findByNickname("딸기").get().getId();
+        Long inviteeId = userRepository.findByNickname("당근").get().getId();
+
+        // 일정 생성
+        mvc.perform(post("/events").header("Authorization", hostToken)
+                .contentType(APPLICATION_JSON)
+                .content("{\"date\": \"2023-03-15\", " +
+                        "\"time\":\"12:00:00\"," +
+                        "\"subject\":\"과일 정기 모임\"," +
+                        "\"content\":\"채소가게에서 저녁 식사\", " +
+                        "\"participantsId\": " +
+                        "[" + hostId + "," + inviteeId + "]}"));
+    }
+
+    @AfterEach
+    void afterEach() {
+        scheduleRepository.deleteAll();
+        eventRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @DisplayName("삭제된 일정을 수락하려는 경우 " +
+            "400 상태 코드와 응답 메시지 반환 " +
+            "scheduleStatus는 기존(PENDING)과 동일하다.")
+    @Test
+    void 일정_수락_실패_케이스1() throws Exception {
+        Event event = eventRepository.findBySubject("과일 정기 모임").get();
+        event.removeEvent();
+        Long eventId = event.getId();
+
+        Long inviteeId = userRepository.findByNickname("당근").get().getId();
+
+        //when
+        mvc.perform(post( "/events/" + eventId + "/acceptance")
+                .header("Authorization", inviteeToken)
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        Schedule schedule = scheduleRepository.findByUser_IdAndEvent_Id(inviteeId, eventId).get();
+        Assertions.assertThat(schedule.getScheduleStatus()).isEqualTo(PENDING);
+    }
+
+    @DisplayName("이미 수락 혹은 거절된을 수락하려는 경우 " +
+            "400 상태 코드와 응답 메시지가 반환된다.")
+    @Test
+    void 일정_수락_실패_케이스2() throws Exception {
+        Event event = eventRepository.findBySubject("과일 정기 모임").get();
+        event.removeEvent();
+        Long eventId = event.getId();
+
+        Long inviteeId = userRepository.findByNickname("당근").get().getId();
+
+        //when
+        mvc.perform(post( "/events/" + eventId + "/acceptance")
+                        .header("Authorization", inviteeToken)
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        Schedule schedule = scheduleRepository.findByUser_IdAndEvent_Id(inviteeId, eventId).get();
+        Assertions.assertThat(schedule.getScheduleStatus()).isEqualTo(PENDING);
+    }
+
+    /**
+     * 로직 개선 필요.
+     */
+    @DisplayName("초대 받지 않은 스케줄을 수락하는 경우 " +
+            "500에러 발생")
+    @Test
+    void 일정_수락_실패_케이스3() throws Exception {
+
+    }
+
+    @DisplayName("이미 기한이 지난 일정을 수락하려는 경우 " +
+            "???")
+    @Test
+    void 일정_수락_실패_케이스4() throws Exception {
+
+    }
+
+    @DisplayName("내가 만든 일정을 수략하려는 경우 " +
+            "상태 코드 400 발생" +
+            "ScheduleStatus 필드는 ACCEPT로 유지된다.")
+    @Test
+    void 일정_수락_실패_케이스5() throws Exception {
+        Long hostId = userRepository.findByNickname("딸기").get().getId();
+
+        Long eventId = eventRepository.findBySubject("과일 정기 모임").get().getId();
+        Schedule initialSchedule = scheduleRepository.findByUser_IdAndEvent_Id(hostId, eventId).get();
+        // 스케줄을 수락 전 ScheduleStatus 필드
+        Assertions.assertThat(initialSchedule.getScheduleStatus()).isEqualTo(ACCEPT);
+        //when
+        mvc.perform(post( "/events/" + eventId + "/acceptance")
+                        .header("Authorization", hostToken)
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        Schedule acceptedSchedule = scheduleRepository.findByUser_IdAndEvent_Id(hostId, eventId).get();
+        Assertions.assertThat(acceptedSchedule.getScheduleStatus()).isEqualTo(ACCEPT);
+    }
+
+    @DisplayName("일정에 초대받은 사용자가 일정 수락 시, 수락한 사용자의 Schedule 엔티티의" +
+            "ScheduleStatus 필드 PENDING -> ACCEPT 로 변경되고" +
+            "다른 사용자의 ScheduleStatus 필드에는 영향을 미치지 않는다.")
+    @Test
+    void 일정_수락_성공_케이스() throws Exception {
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        transactionManager.commit(status);
+
+        Long hostId = userRepository.findByNickname("딸기").get().getId();
+        Long inviteeId = userRepository.findByNickname("당근").get().getId();
+        //given
+
+        Long eventId = eventRepository.findBySubject("과일 정기 모임").get().getId();
+        Schedule initialSchedule1 = scheduleRepository.findByUser_IdAndEvent_Id(inviteeId, eventId).get();
+        // 스케줄을 수락 전 ScheduleStatus 필드
+        Assertions.assertThat(initialSchedule1.getScheduleStatus()).isEqualTo(PENDING);
+
+        //when
+        mvc.perform(post( "/events/" + eventId + "/acceptance")
+                .header("Authorization", inviteeToken)
+                .contentType(APPLICATION_JSON));
+
+        //then
+        // 스케줄을 수락 후 ScheduleStatus 필드
+        Schedule acceptedSchedule = scheduleRepository.findByUser_IdAndEvent_Id(inviteeId, eventId).get();
+        Assertions.assertThat(acceptedSchedule.getScheduleStatus()).isEqualTo(ACCEPT);
+    }
+
+    private void makeUser() throws Exception {
         MockMultipartFile file1 = new MockMultipartFile(
                 "data", "", "application/json",
                 "{\"nickname\" : \"딸기\",\"username\": \"이땡땡\", \"email\": \"ddalgi@gmail.com\", \"password\": \"123456aA9\"}".getBytes());
@@ -68,75 +208,12 @@ public class ScheduleAcceptTest {
                 "{\"nickname\" : \"수박\",\"username\": \"박땡땡\", \"email\": \"suback@gmail.com\", \"password\": \"123456aA9\"}".getBytes());
         mvc.perform(multipart("/auth/signup")
                 .file(file3));
+    }
 
-        MockMultipartFile file4 = new MockMultipartFile(
-                "data", "", "application/json",
-                "{\"nickname\" : \"참외\",\"username\": \"최땡땡\", \"email\": \"chamwhe@gmail.com\", \"password\": \"123456aA9\"}".getBytes());
-        mvc.perform(multipart("/auth/signup")
-                .file(file4));
-
-        MvcResult result = mvc.perform(post("/auth/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"email\": \"ddalgi@gmail.com\", \"password\": \"123456aA9\"}")).andReturn();
-
-        hostToken = result.getResponse().getHeader("Authorization");
-
+    private MvcResult getLoginResult(String content) throws Exception {
         MvcResult result2 = mvc.perform(post("/auth/login")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"email\": \"danguen@gmail.com\", \"password\": \"123456aA9\"}")).andReturn();
-
-        inviteeToken = result2.getResponse().getHeader("Authorization");
-    }
-
-    @AfterEach
-    void afterEach() {
-        scheduleRepository.deleteAll();
-        eventRepository.deleteAll();
-        userRepository.deleteAll();
-    }
-
-
-    @DisplayName("일정 수락 시 " +
-            "일정의 수락한 사용자의 Schedule 엔티티 isAccepted 필드 false -> true 변화" +
-            "타인의 isAccepted 필드는 그대로 false 반환")
-    @Test
-    void test1() throws Exception {
-        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-        transactionManager.commit(status);
-
-        //given
-        Long hostId = userRepository.findByNickname("딸기").get().getId();
-        Long inviteeId1 = userRepository.findByNickname("당근").get().getId();
-        Long inviteeId2 = userRepository.findByNickname("수박").get().getId();
-
-        mvc.perform(post("/events").header("Authorization", hostToken)
-                .contentType(APPLICATION_JSON)
-                .content("{\"date\": \"2023-03-15\", " +
-                        "\"time\":\"12:00:00\"," +
-                        "\"subject\":\"과일 정기 모임\"," +
-                        "\"content\":\"채소가게에서 저녁 식사\", " +
-                        "\"participantsId\": " +
-                        "[" + hostId + "," + inviteeId1 + "," + inviteeId2 + "]}"));
-
-        Long eventId = eventRepository.findBySubject("과일 정기 모임").get().getId();
-        Schedule danguenSchedule = scheduleRepository.findByUser_IdAndEvent_Id(inviteeId1, eventId).get();
-        Schedule subackSchedule = scheduleRepository.findByUser_IdAndEvent_Id(inviteeId2, eventId).get();
-
-
-        // 스케줄을 수락하기 전 당근,수박의 isAccepted 필드
-        Assertions.assertThat(danguenSchedule.getScheduleStatus()).isEqualTo(PENDING);
-        Assertions.assertThat(subackSchedule.getScheduleStatus()).isEqualTo(PENDING);
-
-        //when
-        mvc.perform(post( "/events/" + eventId + "/acceptance")
-                .header("Authorization", inviteeToken)
-                .contentType(APPLICATION_JSON));
-
-        //then
-        // 스케줄을 수락 후 당근, 수박의 isAccepted 필드
-        Schedule updateDanguenSchedule = scheduleRepository.findByUser_IdAndEvent_Id(inviteeId1, eventId).get();
-        Schedule updateSubackSchedule = scheduleRepository.findByUser_IdAndEvent_Id(inviteeId2, eventId).get();
-        Assertions.assertThat(updateDanguenSchedule.getScheduleStatus()).isEqualTo(ACCEPT);
-        Assertions.assertThat(updateSubackSchedule.getScheduleStatus()).isEqualTo(PENDING);
+                .content(content)).andReturn();
+        return result2;
     }
 }
