bd5b3226dd92bbb5a2e790a3634a639f567d2170
yuminwoo <ymw1023@naver.com>
2023-02-21 14:22:27 +0900

    Feat #123 필요없는 lombok 제거

--- src/main/java/com/gnims/project/domain/user/dto/AuthEmailDto.java
+++ src/main/java/com/gnims/project/domain/user/dto/AuthEmailDto.java
@@ -1,10 +1,8 @@
 package com.gnims.project.domain.user.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class AuthEmailDto {
     private String email;
 }
