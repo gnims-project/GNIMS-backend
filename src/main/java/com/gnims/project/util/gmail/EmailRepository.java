b4614e9497510afc8933926fb362e8915db4e688
yuminwoo <ymw1023@naver.com>
2023-02-23 00:41:26 +0900

    Feat #123 에러 수정

--- src/main/java/com/gnims/project/util/gmail/EmailRepository.java
+++ src/main/java/com/gnims/project/util/gmail/EmailRepository.java
@@ -1,6 +1,7 @@
 package com.gnims.project.util.gmail;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -8,5 +9,7 @@ import java.util.Optional;
 public interface EmailRepository extends JpaRepository<EmailValidation, Long> {
 //    Optional<EmailValidation> findByCode(String code);
     Optional<EmailValidation> findByEmail(String email);
+
+    @Transactional
     void deleteByCreateAtBefore(LocalDateTime time);
 }
