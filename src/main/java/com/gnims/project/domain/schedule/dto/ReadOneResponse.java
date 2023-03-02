ff0cada2b1c22d86f3dd44476c2b408015696340
JxxHxxx <leesin5498@naver.com>
2023-03-02 12:32:51 +0900

    Refactor #175 사용하지 않는 패키지, 생성자 수정

--- src/main/java/com/gnims/project/domain/schedule/dto/ReadOneResponse.java
+++ src/main/java/com/gnims/project/domain/schedule/dto/ReadOneResponse.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Getter
@@ -18,18 +17,6 @@ public class ReadOneResponse {
     private Long hostId;
     private Long dDay;
     private List<ReadOneUserDto> invitees;
-
-    // proto타입을 위해 잠시 존재
-    public ReadOneResponse(Long eventId, LocalDate date, LocalTime time, String cardColor, String subject, String content, Long dDay, List<ReadOneUserDto> invitees) {
-        this.eventId = eventId;
-        this.date = date;
-        this.time = time;
-        this.cardColor = cardColor;
-        this.subject = subject;
-        this.content = content;
-        this.dDay = dDay;
-        this.invitees = invitees;
-    }
 
     public ReadOneResponse(Long eventId, LocalDate date, LocalTime time, String cardColor, String subject, String content, Long hostId, Long dDay, List<ReadOneUserDto> invitees) {
         this.eventId = eventId;
