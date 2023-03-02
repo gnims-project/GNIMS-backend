0ce0d9c6f586a2da9d1b17b2c5fdcdadb14d68f2
JxxHxxx <leesin5498@naver.com>
2023-03-02 10:50:50 +0900

    Refactor #175 헬스 체크 API 도메인 패키지에서 Share 패키지로 이동

--- src/main/java/com/gnims/project/share/health/HealthCheckController.java
+++ src/main/java/com/gnims/project/share/health/HealthCheckController.java
@@ -1,4 +1,4 @@
-package com.gnims.project.domain.friendship.controller;
+package com.gnims.project.share.health;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
