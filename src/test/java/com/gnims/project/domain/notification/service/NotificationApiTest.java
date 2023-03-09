93b6e2f548336a83392b9290412dfcbdb2a78880
JxxHxxx <leesin5498@naver.com>
2023-03-10 01:16:47 +0900

    Test #272 알림 상세 조회 API 올바르지 못한 요청 테스트 케이스

--- src/test/java/com/gnims/project/domain/notification/service/NotificationApiTest.java
+++ src/test/java/com/gnims/project/domain/notification/service/NotificationApiTest.java
@@ -23,8 +23,10 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static com.gnims.project.domain.notification.entity.NotificationType.*;
+import static com.gnims.project.share.message.ExceptionMessage.BAD_ACCESS;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -68,7 +70,7 @@ class NotificationApiTest {
             "사용자의 Id로 된 key를 가진 SseEmitter 가 생성된다. " +
             "연결을 요청하지 않은 사용자는 SseEmitter 가 존재하지 않는다.")
     @Test
-    void test1() throws Exception {
+    void SSE_연결_테스트() throws Exception {
         //when - SSE 연결
         mvc.perform(get("/connect").header("Authorization", inviteeToken));
         User connectUser = userRepository.findByNickname("당근").get();
@@ -80,6 +82,87 @@ class NotificationApiTest {
 
     }
 
+    @DisplayName("알림 전체 조회 성공 시, " +
+            "200 상태코드 반환 " +
+            "알림 갯수만큼 응답한다.")
+    @Test
+    void 알림전체조회_테스트() throws Exception {
+        User user = userRepository.findByNickname("딸기").get();
+        Long createBy = userRepository.findByNickname("당근").get().getId();
+
+        Notification notificationA = new Notification(user, createBy, "테스트 알림1");
+        Notification notificationB = new Notification(user, createBy, "테스트 알림2");
+
+        notificationRepository.save(notificationA);
+        notificationRepository.save(notificationB);
+
+        mvc.perform(get("/notifications").header("Authorization", hostToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.size()").value(2));
+    }
+
+    @DisplayName("알림 상세 조회 시 " +
+            "Notification 엔티티 isChecked 필드는 False -> True 로 변경된다.")
+    @Test
+    void 알림상세조회_성공케이스() throws Exception {
+        User sender = userRepository.findByNickname("당근").get();
+        Long createBy = userRepository.findByNickname("딸기").get().getId();
+        //given - 딸기가 당근에게 팔로우했을 때
+        Notification notification = new Notification(sender, createBy, "테스트 메시지");
+        Notification saveNotification = notificationRepository.save(notification);
+
+        //알림을 확인하기 전 isChecked 필드
+        Assertions.assertThat(saveNotification.getIsChecked()).isFalse();
+        //알림이 생긴다.
+        mvc.perform(get("/notifications/" + saveNotification.getId())
+                        .header("Authorization", inviteeToken))
+                .andExpect(status().isOk());
+
+        Notification updateNotification = notificationRepository.findAllByUserId(sender.getId()).get(0);
+        //알림을 확인후
+        Assertions.assertThat(updateNotification.getIsChecked()).isTrue();
+    }
+
+    @DisplayName("존재하지 않는 알람을 상세 조회 시 " +
+            "상태 코드 400 " +
+            "메시지 {BAD_ACCESS} 반환")
+    @Test
+    void 알림상세조회_실패케이스1() throws Exception {
+        User sender = userRepository.findByNickname("당근").get();
+        Long createBy = userRepository.findByNickname("딸기").get().getId();
+        //given - 딸기가 당근에게 팔로우했을 때
+        Notification notification = new Notification(sender, createBy, "테스트 메시지");
+        notificationRepository.save(notification);
+
+        //알림이 생긴다.
+        mvc.perform(get("/notifications/" + Long.MAX_VALUE)
+                        .header("Authorization", inviteeToken))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(BAD_ACCESS));
+
+    }
+
+    @DisplayName("내게 오지 않은 알림 상세 조회 시 " +
+            "상태 코드 400 " +
+            "메시지 {BAD_ACCESS} 반환")
+    @Test
+    void 알림상세조회_실패케이스2() throws Exception {
+        User sender = userRepository.findByNickname("당근").get();
+        Long createBy = userRepository.findByNickname("딸기").get().getId();
+        //given - 딸기가 당근에게 팔로우했을 때
+        Notification notification = new Notification(sender, createBy, "테스트 메시지");
+        Notification saveNotification = notificationRepository.save(notification);
+
+        mvc.perform(get("/notifications/" + saveNotification.getId())
+                        .header("Authorization", hostToken)) // hostToken 을 보유한 사용자에게 온 알림이 아니다.
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(BAD_ACCESS));
+
+        Notification updateNotification = notificationRepository.findAllByUserId(sender.getId()).get(0);
+        //알림을 확인후
+        Assertions.assertThat(updateNotification.getIsChecked()).isFalse();
+    }
+
     @DisplayName("일정에 초대받을 경우, " +
             "SSE 연결 여부와 상관없이 알림(Notification)이 저장된다.Notification 에는 " +
             "알림을 보낸이(CreatedBy) " +
@@ -87,7 +170,7 @@ class NotificationApiTest {
             "알림 타입(Notification) - SCHEDULE 이 저장된다.")
     @Disabled("비동기 처리로 바꾼 뒤 오류 - 원인 파악 중 - 포스트맨으로 API 테스트하는 경우에는 정상 작동")
     @Test
-    void test2() throws Exception {
+    void 알림비동기처리_테스트1() throws Exception {
         //given
         Long hostId = userRepository.findByNickname("딸기").get().getId();
         Long inviteeId = userRepository.findByNickname("당근").get().getId();
@@ -108,7 +191,7 @@ class NotificationApiTest {
             "알림 타입(Notification) - FRIENDSHIP 이 저장된다.")
     @Disabled("비동기 처리로 바꾼 뒤 오류 - 원인 파악 중 - 포스트맨으로 API 테스트하는 경우에는 정상 작동")
     @Test
-    void test3() throws Exception {
+    void 알림비동기처리_테스트2() throws Exception {
 
         Long hostId = userRepository.findByNickname("딸기").get().getId();
         Long followId = userRepository.findByNickname("당근").get().getId();
@@ -123,35 +206,11 @@ class NotificationApiTest {
         Assertions.assertThat(notification.getNotificationType()).isEqualTo(FRIENDSHIP);
     }
 
-    @DisplayName("알림 상세 조회 시 " +
-            "Notification 엔티티 isChecked 필드는 False -> True 로 변경된다.")
-    @Test
-    void test4() throws Exception {
-        User sender = userRepository.findByNickname("당근").get();
-        Long createBy = userRepository.findByNickname("딸기").get().getId();
-        //given - 딸기가 당근에게 팔로우했을 때
-        Notification notification = new Notification(sender, createBy, "테스트 메시지");
-        Notification saveNotification = notificationRepository.save(notification);
-
-        //알림을 확인하기 전 isChecked 필드
-        Assertions.assertThat(saveNotification.getIsChecked()).isFalse();
-        //알림이 생긴다.
-        mvc.perform(get("/notifications/" + saveNotification.getId())
-                        .header("Authorization", inviteeToken))
-                .andExpect(status().isOk());
-
-        Notification updateNotification = notificationRepository.findAllByUserId(sender.getId()).get(0);
-        //알림을 확인후
-        Assertions.assertThat(updateNotification.getIsChecked()).isTrue();
-
-
-    }
-
     @DisplayName("팔로우 알림은 최초의 팔로우 관계가 맺어질 때만 발생한다. " +
             "최초의 팔로우(FollowStatus : INIT)를 맺을 때만 생성되고" +
             "취소(INACTIVE) -> 다시 맺을 경우(ACTIVE) 알림이 생성되지 않는다.")
     @Test
-    void test5() throws Exception {
+    void 알림비동기처리_테스트3() throws Exception {
         //given
         Long followId = userRepository.findByNickname("당근").get().getId();
         //when 팔로우 요청을 3번 보낸다. INIT - INACTIVE - ACTIVE
@@ -168,7 +227,7 @@ class NotificationApiTest {
             "자신이 받은 알림의 갯수만큼 알림이 존재해야 한다. " +
             "테스트 케이스에서는 총 10번의 일정 초대를 보내기 때문에 알림을 조회할 경우 10개의 알림이 존재해야 한다.")
     @Test
-    void test6() throws Exception {
+    void 알림비동기처리_테스트4() throws Exception {
         //given
         Long hostId = userRepository.findByNickname("딸기").get().getId();
         Long inviteeId = userRepository.findByNickname("당근").get().getId();
@@ -181,9 +240,9 @@ class NotificationApiTest {
         Assertions.assertThat(count).isEqualTo(10l);
     }
 
-    @DisplayName("알림 모두 읽음 처리")
+    @DisplayName("알림 모두 읽음 처리가 실행되면 알림 엔티티의 isChecked 필드는 false -> true 값으로 변경된다.")
     @Test
-    void test8() throws Exception {
+    void 알림모두읽음_성공케이스() throws Exception {
         User user = userRepository.findByNickname("딸기").get();
         Long createBy = userRepository.findByNickname("당근").get().getId();
 
@@ -198,6 +257,25 @@ class NotificationApiTest {
         List<Notification> notifications = notificationRepository.findAllByUserId(user.getId());
 
         notifications.forEach(n -> Assertions.assertThat(n.getIsChecked()).isTrue());
+    }
+
+    @DisplayName("올바른 토큰을 가지고 있지 않으면 isChecked 필드는 false 에서 변경되지 않는다.")
+    @Test
+    void 알림모두읽음_실패케이스() throws Exception {
+        User user = userRepository.findByNickname("딸기").get();
+        Long createBy = userRepository.findByNickname("당근").get().getId();
+
+        Notification notificationA = new Notification(user, createBy, "테스트 알림1");
+        Notification notificationB = new Notification(user, createBy, "테스트 알림2");
+
+        notificationRepository.save(notificationA);
+        notificationRepository.save(notificationB);
+
+        // hostToken 을 가진 유저에게 온 알림이다.
+        mvc.perform(put("/notifications").header("Authorization", inviteeToken));
+
+        List<Notification> notifications = notificationRepository.findAllByUserId(user.getId());
+        notifications.forEach(n -> Assertions.assertThat(n.getIsChecked()).isFalse());
     }
 
     private void createSchedule(Long hostId, Long inviteeId) throws Exception {
