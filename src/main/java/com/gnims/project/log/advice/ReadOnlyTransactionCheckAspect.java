0cd67a3ef0c4bd80c457fe262855df5eea765372
JxxHxxx <leesin5498@naver.com>
2023-03-30 21:23:33 +0900

    Improvement #312 읽기 전용 트랜잭션 체크 AOP

---
+++ src/main/java/com/gnims/project/log/advice/ReadOnlyTransactionCheckAspect.java
@@ -0,0 +1,27 @@
+package com.gnims.project.log.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+@Aspect
+@Component
+public class ReadOnlyTransactionCheckAspect {
+
+    @Around("execution(* com.gnims.project.domain..*Service.read*(..))")
+    public void checkReadOnly(ProceedingJoinPoint joinPoint) throws Throwable {
+        String methodName = joinPoint.getSignature().getName();
+        joinPoint.proceed();
+
+        if (TransactionSynchronizationManager.isCurrentTransactionReadOnly()) {
+            log.info("[{} Method is readOnly]", methodName);
+            return;
+        }
+
+        log.info("[{} Method is Not readOnly]", methodName);
+    }
+}
